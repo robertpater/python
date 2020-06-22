b4e5614793a08e4ab93f6e5a784f3cbdefcda8e2
Rob <regm1978@gmail.com>
2020-06-22 07:41:11 +0200

    lista

--- zgaduj.py
+++ zgaduj.py
@@ -1,18 +1,26 @@
+
+
 import random
-wymyslona = random.randint(0,100)
+wymyslona = random.randint(0, 100)
 print('Wymyslilem liczbe')
 print(wymyslona)
 
 licznik = 0
 
-while(1):
-	licznik+=1
-	liczba = int(input('Podaj liczbe od 0 do 100: '))
 
-	if liczba == wymyslona:
-		print('w koncu udalo sie !! liczba prob= ',licznik)
-		break
-	if liczba < wymyslona:
-		print('za malo')
-	else:
-		print('za duzo')
+while(1):
+    licznik += 1
+    liczba = int(input('Podaj liczbe od 0 do 100: '))
+
+    if liczba == wymyslona:
+        print('w koncu udalo sie !! liczba prob= ', licznik)
+        break
+
+    if liczba < wymyslona:
+        print('za malo')
+    else:
+        print('za duzo')
+
+
+for num in [1, 2, 3, 4]:
+    print(n)
